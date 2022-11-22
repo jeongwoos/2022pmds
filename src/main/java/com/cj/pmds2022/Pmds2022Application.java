f871b39c5e7cc22bc933b99f54f35dbee63044ce
jeongwoos <hingofm@naver.com>
2022-11-22 10:02:55 +0900

    initialize pmds2022-project

---
+++ src/main/java/com/cj/pmds2022/Pmds2022Application.java
@@ -0,0 +1,13 @@
+package com.cj.pmds2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pmds2022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pmds2022Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cj/pmds2022/Pmds2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cj.pmds2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pmds2022ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
